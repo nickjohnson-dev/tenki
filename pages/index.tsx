74ad19ab4b72ac400c33aec7d08f56d08b3bd726
Nick Johnson <nickjohnson.dev@gmail.com>
2022-07-02 08:56:01 -0500

    Initial round of installs and setup.

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { Box, Center } from '@mantine/core';
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <Center>Test</Center>
+    </Box>
+  );
+};
+
+export default Home;
