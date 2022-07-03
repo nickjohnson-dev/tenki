0e2ffe741b6a47182b655cac67b89895ef10091a
Nick Johnson <nickjohnson.dev@gmail.com>
2022-07-02 19:38:06 -0500

    Fix linting and fetch some sample forecast data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,34 @@
-import { Box, Center } from '@mantine/core';
+import { Center } from '@mantine/core';
 import type { NextPage } from 'next';
+import { useEffect } from 'react';
+import useSWR from 'swr';
 
 const Home: NextPage = () => {
+  const {
+    data: forecasts,
+    error,
+    isValidating,
+  } = useSWR('forecasts', async () => {
+    const data = await fetch(
+      'https://api.weather.gov/gridpoints/HUN/65,34/forecast/hourly',
+    ).then((res) => res.json());
+
+    if (!data) {
+      throw new Error('Failed to fetch forecasts.');
+    }
+
+    return data;
+  });
+
+  useEffect(() => {
+    console.log({ forecasts });
+  }, [forecasts]);
+
   return (
     <Center sx={{ minHeight: '100vh' }}>
-      <div>Test</div>
+      {isValidating && <div>Loading...</div>}
+      {!isValidating && error && <div>Error: {error.message}</div>}
+      {!isValidating && !error && <div>Test</div>}
     </Center>
   );
 };
